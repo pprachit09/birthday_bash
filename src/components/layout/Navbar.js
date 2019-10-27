3798864e0f41de82ce6514505c7f424ba5df2b4d
pprachit09 <pprachit09@gmail.com>
2019-10-27 19:04:38 +0530

    adding Footer

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper blue">
-      <div className="container">
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">Coderlust</Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
+    <header>
+      <nav className="nav-wrapper blue">
+        <div className="container">
+          <div className="nav-wrapper">
+            <Link to="/" className="brand-logo">Coderlust</Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   )
 }
 
